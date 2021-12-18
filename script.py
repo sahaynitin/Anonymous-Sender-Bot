3a84113b382c5e00cb30344bcd640b6acbc93f96
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-18 11:00:09 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,60 @@
 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+class Script(object):
+
+
+
+    START_TEXT = """
+Hey {} 
+
+I am Telegram Anonymous Sender Bot
+
+I can help you to remove caption and tag from media
+
+Use Help Command to Know How to Use me
+
+Made With 💕 By @Tellybots_4u
+"""
+    HELP_TEXT = """
+Recommended
+➠ Just Send media To Remove Caption
+
+Recommended
+➠ Just readd a Caption to add Caption
+
+Made With 💕 By @Tellybots_4u
+"""
+    ABOUT_TEXT = """
+ **🤖 Bot :** Anonymous-Sender\n
+ **👲 Developer :** [Tellybots_4u](https://telegram.me/tellybots_4u)\n
+ **👥 Channel :** [Tellybots_4u](https://telegram.me/tellybots_4u)\n
+ **❄️ Credits :** Everyone in this journey\n
+ **🍴 Source :** [Click here](https://t.me/tellybots_digital)\n
+ **📝 Language :** [Python3](https://python.org)\n
+ **📚 Library :** [Pyrogram v1.2.0](https://pyrogram.org)\n
+ **🌟 Server :** [Heroku](https://heroku.com)\n
+"""
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤖 Update Channel', url='https://telegram.me/tellybots_4u'),
+        InlineKeyboardButton('💬 Support Group', url='https://telegram.me/tellybots_support')
+        ],[
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('👲 About', callback_data='about'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
