3cf5753e45fcb1189c1a07ab07a548c124c409c7
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-19 12:13:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,29 @@ async def start(bot, update):
         disable_web_page_preview=True,
         reply_markup=Script.START_BUTTONS
     )
-@
+@Tellybots.on_callback_query()
+async def button(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=Script.START_TEXT.format(update.from_user.mention),
+            reply_markup=Script.START_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=Script.HELP_TEXT,
+            reply_markup=Script.HELP_BUTTONS,
+            disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=Script.ABOUT_TEXT.format((await bot.get_me()).username),
+            reply_markup=Script.ABOUT_BUTTONS,
+            disable_web_page_preview=True
+        )
+    else:
+        await update.message.delete()
+
  
 @Tellybots.on_message(filters.private & ~filters.edited & ~filters.command(["start"]))
 async def copy(_, msg):
